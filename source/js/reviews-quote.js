4c75d0c5117e630f7a1ad354e6c6e2c3870ee628
Artem Zharenkov <zharenkov3@yandex.ru>
2021-06-13 13:02:31 +0300

    edit

--- source/js/reviews-quote.js
+++ source/js/reviews-quote.js
@@ -1,12 +1,14 @@
 $('.reviews-quote__list').slick({
   dots: false,
+  arrows: false,
   mobileFirst: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint: 769,
+      breakpoint: 1250,
       settings: "unslick"
     },
   ]
 });
+
--- source/js/services.js
+++ source/js/services.js
@@ -1,12 +1,43 @@
 $('.services-gallery__list').slick({
+  arrows: true,
   dots: true,
   mobileFirst: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   responsive: [
-    {
-      breakpoint: 769,
-      settings: "unslick"
-    },
+  {
+    breakpoint: 320,
+    settings: {
+      arrows: false
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      arrows: true
+    }
+  },
+
+  {
+    breakpoint: 1250,
+    settings: "unslick"
+  },
+
   ]
 });
+
+// $(function() {
+//   $('.slider-list').slick({
+//     arrows: true,
+//     dots: true,
+//     // autoplay: 2000,
+//     dotsClass: 'reviews-toggles',
+//     responsive: [{
+//       breakpoint: 960,
+//       settings: {
+//         arrows : false
+//       }
+//     }, ]
+//   });
+// });
+
